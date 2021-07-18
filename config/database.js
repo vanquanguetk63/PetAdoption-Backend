5da949a3399d920ef841238750214f03c8af7039
BunQ707 <44667651+BunQ707@users.noreply.github.com>
2021-07-18 10:32:06 +0700

    xóa 1 thư mục không dùng

--- config/database.js
+++
@@ -1,7 +0,0 @@
-// const mongoose = require('mongoose');
-
-// const dbUrl = "mongodb://localhost/eadb"
-
-// mongoose.connect(dbUrl,{ useNewUrlParser: true ,useUnifiedTopology: true})
-//     .then(() => console.log('DB connected...'))
-//     .catch(err => console.log('Cannot connect to DB...', err));
